68afd010c3534d7438f210ebc20751746990d043
woomsgdev <woomsgdev@1c5293ca-d401-11de-9aee-2b7272db14c4>
2009-12-11 16:51:25 +0000

    create dev1

---
+++ trunk/dev1/woomsg/src/woomsg_app.erl
@@ -0,0 +1,22 @@
+%% @author author <author@example.com>
+%% @copyright YYYY author.
+
+%% @doc Callbacks for the woomsg application.
+
+-module(woomsg_app).
+-author('author <author@example.com>').
+
+-behaviour(application).
+-export([start/2,stop/1]).
+
+
+%% @spec start(_Type, _StartArgs) -> ServerRet
+%% @doc application start callback for woomsg.
+start(_Type, _StartArgs) ->
+    woomsg_deps:ensure(),
+    woomsg_sup:start_link().
+
+%% @spec stop(_State) -> ServerRet
+%% @doc application stop callback for woomsg.
+stop(_State) ->
+    ok.
