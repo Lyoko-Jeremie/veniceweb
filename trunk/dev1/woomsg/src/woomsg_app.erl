6f50b12639dfb4193cf8ca7168991672be6ff698
woomsgdev <woomsgdev@1c5293ca-d401-11de-9aee-2b7272db14c4>
2009-12-23 12:14:15 +0000

    test

--- trunk/dev1/woomsg/src/woomsg_app.erl
+++ trunk/dev1/woomsg/src/woomsg_app.erl
@@ -13,10 +13,19 @@
 %% @spec start(_Type, _StartArgs) -> ServerRet
 %% @doc application start callback for woomsg.
 start(_Type, _StartArgs) ->
+    %% add init code here:
+    %% - start - 
+
+    %% - end - 
+
     woomsg_deps:ensure(),
     woomsg_sup:start_link().
 
 %% @spec stop(_State) -> ServerRet
 %% @doc application stop callback for woomsg.
 stop(_State) ->
+    %% add cleanup code here:
+    %% - start - 
+
+    %% - end - 
     ok.
