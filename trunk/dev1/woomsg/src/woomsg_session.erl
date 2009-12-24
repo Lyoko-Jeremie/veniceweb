deb39bd64b29c2522325bba8ef0e815ff1329717
woomsgdev <woomsgdev@1c5293ca-d401-11de-9aee-2b7272db14c4>
2009-12-24 08:02:56 +0000

    test

--- trunk/dev1/woomsg/src/woomsg_session.erl
+++ trunk/dev1/woomsg/src/woomsg_session.erl
@@ -1,3 +1,24 @@
 -module(woomsg_session).
--export([]).
+-export([set/1, get/1]).
+
+
+%% 成功返回: {atomic, ok}
+set(Key) ->
+    Val = woomsg_util:get_guid(),
+    F = fun() ->
+		mnesia:write({session, Key, Val})
+	end,
+    mnesia:transaction(F).
+
+
+%% 成功返回: {atomic, Val}
+%% 失败返回: {atomic,{error,key_not_found}}
+get(Key) ->
+    db:get(session, Key, session_id).
+
+
+
+
+
+
 
