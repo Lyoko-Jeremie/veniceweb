a40eccf540072cb8bb501d318d3f17ff7ce74e12
woomsgdev <woomsgdev@1c5293ca-d401-11de-9aee-2b7272db14c4>
2009-12-28 13:10:11 +0000

    test

--- dev1/woomsg/src/controller/index_controller.erl
+++ dev1/woomsg/src/controller/index_controller.erl
@@ -2,6 +2,10 @@
 -export([handle_get/2, handle_post/2]).
 
 handle_get(Req, _DocRoot) ->
+    Username = woomsg_cookie:parse_cookie_username(Req),
+    Remember = woomsg_cookie:parse_cookie_remember(Req),
+    
+    
     Data = index_view:index("test data"),
     Req:respond({200, [{"Content-Type","text/html"}], Data}).
 
