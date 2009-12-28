725190a86f305bdf84bc70bd1afcd8a18c4d1a8b
woomsgdev <woomsgdev@1c5293ca-d401-11de-9aee-2b7272db14c4>
2009-12-28 03:04:07 +0000

    test

--- trunk/dev1/woomsg/server_config.sh
+++ trunk/dev1/woomsg/server_config.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd `dirname $0`
+echo "create mnesia cluster"
 mkdir $1/mnesia
 mkdir $1/mnesia/dbnodemaster
 mkdir $1/mnesia/dbnodeslave1
@@ -7,5 +8,31 @@ mkdir $1/mnesia/dbnodeslave2
 mkdir $1/mnesia/dbnodeslave3
 mkdir $1/mnesia/dbnodeslave4
 mkdir $1/mnesia/servernode
+
+echo "create error logs"
 mkdir $1/error_logs
 mkdir $1/error_logs_dev
+
+echo "create fs"
+mkdir $1/fs
+
+echo "create photo"
+mkdir $1/fs/photo
+mkdir $1/fs/photo/node1
+mkdir $1/fs/photo/node2
+mkdir $1/fs/photo/node3
+mkdir $1/fs/photo/node4
+
+echo "create photo/sys"
+mkdir $1/fs/photo/sys
+mkdir $1/fs/photo/sys/path-guid
+mkdir $1/fs/photo/sys/path-guid/mini
+mkdir $1/fs/photo/sys/path-guid/normal
+
+echo "create pic"
+mkdir $1/fs/pic
+mkdir $1/fs/pic/node1
+mkdir $1/fs/pic/node2
+mkdir $1/fs/pic/node3
+mkdir $1/fs/pic/node4
+echo "complete!"
