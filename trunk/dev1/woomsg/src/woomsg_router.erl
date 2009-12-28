94c541166e0873713b0875b45ac53ef51ac959da
woomsgdev <woomsgdev@1c5293ca-d401-11de-9aee-2b7272db14c4>
2009-12-28 05:44:17 +0000

    test

--- trunk/dev1/woomsg/src/woomsg_router.erl
+++ trunk/dev1/woomsg/src/woomsg_router.erl
@@ -11,6 +11,22 @@ handle_request(Req, DocRoot) ->
 		    index_controller:handle_get(Req, DocRoot);
 		"index" ->
 		    index_controller:handle_get(Req, DocRoot);
+                "api" ->
+		    api_controller:handle_get(Req, DocRoot);
+		"faq" ->
+		    faq_controller:handle_get(Req, DocRoot);
+		"publictimeline" ->
+		    publictimeline_controller:handle_get(Req, DocRoot);
+		"setting" ->
+		    setting_controller:handle_get(Req, DocRoot);
+		"support" ->
+		    support_controller:handle_get(Req, DocRoot);
+		"tag" ->
+		    tag_controller:handle_get(Req, DocRoot);
+		"upload" ->
+		    upload_controller:handle_get(Req, DocRoot);
+		"user" ->
+		    user_controller:handle_get(Req, DocRoot);
                 _ ->
                     Req:serve_file(Path, DocRoot)
             end;
