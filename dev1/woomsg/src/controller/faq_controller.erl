66a5a32ddafebaa7480cf7e84a12965e3377c3dc
woomsgdev <woomsgdev@1c5293ca-d401-11de-9aee-2b7272db14c4>
2009-12-28 05:44:17 +0000

    test

--- dev1/woomsg/src/controller/faq_controller.erl
+++ dev1/woomsg/src/controller/faq_controller.erl
@@ -1,8 +1,8 @@
--module(index_controller).
+-module(faq_controller).
 -export([handle_get/2, handle_post/2]).
 
 handle_get(Req, _DocRoot) ->
-    Data = index_view:index("test data"),
+    Data = faq_view:index("test data"),
     Req:respond({200, [{"Content-Type","text/html"}], Data}).
 
 handle_post(_Req, _DocRoot) ->
--- dev1/woomsg/src/controller/tag_controller.erl
+++ dev1/woomsg/src/controller/tag_controller.erl
@@ -1,10 +1,9 @@
--module(index_controller).
+-module(tag_controller).
 -export([handle_get/2, handle_post/2]).
 
 handle_get(Req, _DocRoot) ->
-    Data = index_view:index("test data"),
+    Data = tag_view:index("test data"),
     Req:respond({200, [{"Content-Type","text/html"}], Data}).
 
 handle_post(_Req, _DocRoot) ->
     ok.
-
