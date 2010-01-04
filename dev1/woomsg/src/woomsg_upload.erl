9b4cf081099475b78ab43cb13267d1befdd8b8b0
woomsgdev <woomsgdev@1c5293ca-d401-11de-9aee-2b7272db14c4>
2010-01-04 05:49:23 +0000

    test

--- dev1/woomsg/src/woomsg_upload.erl
+++ dev1/woomsg/src/woomsg_upload.erl
@@ -1,14 +1,25 @@
 -module(woomsg_upload).
 -include("woomsg_configure.hrl").
--export([parse_form/1, parse_form/2]).
+-export([parse_form_pic/1, parse_form_pic/2, 
+         parse_form_photo/1, parse_form_photo/2,
+         parse_form/3]).
 
 -record(state, {filename, file, path, guid, type, key, value}).
 
-parse_form(Req) ->
-    parse_form(Req, false).
+parse_form_photo(Req) ->
+    parse_form_photo(Req, false).
 
-parse_form(Req, Debug) ->
-    Callback = fun(Next) -> callback(Next, #state{}, [], Debug) end,
+parse_form_pic(Req) ->
+    parse_form_pic(Req, false).
+
+parse_form_photo(Req, Debug) ->
+    parse_form(Req, Debug, fun(State)-> update_state_photo(State) end).
+
+parse_form_pic(Req, Debug) ->
+    parse_form(Req, Debug, fun(State) -> update_state_pic(State) end).
+
+parse_form(Req, Debug, UpdateStateCallback) ->
+    Callback = fun(Next) -> callback(Next, #state{}, [], Debug, UpdateStateCallback) end,
     {_, _, Res} = mochiweb_multipart:parse_multipart_request(Req, Callback),
     Res.
 
@@ -35,7 +46,7 @@ parse_form(Req, Debug) ->
 %% png  -  {"content-type",{"image/png",[]}}
 %% gif  -  {"content-type",{"image/gif",[]}}
 %% bmp  -  {"content-type",{"image/bmp",[]}}
-callback(Next, State, Acc, Debug) ->
+callback(Next, State, Acc, Debug, UpdateStateCallback) ->
     case Next of
         {headers, Headers} ->
             {"form-data", H1} = proplists:get_value("content-disposition", Headers),
@@ -47,7 +58,8 @@ callback(Next, State, Acc, Debug) ->
 			   [] ->
 			       State;
 			    _ ->
-			       #state{filename = "/tmp/" ++ Filename, type=verify_type_in_header(Headers)}
+			       %% 这个文件名会在UpdateStateCallback中修改.
+			       #state{filename = Filename, type=verify_type_in_header(Headers)}
                        end,
 	    case Debug of
 	        true ->
@@ -55,7 +67,7 @@ callback(Next, State, Acc, Debug) ->
 		    io:format("NewState: ~p~n", [NewState]),
                     io:format("NewAcc: ~p~n", [Acc])
 	    end,
-	    fun(N) -> callback(N, NewState, Acc, Debug) end;
+	    fun(N) -> callback(N, NewState, Acc, Debug, UpdateStateCallback) end;
 	{body, Body} ->
 	    if
 	        State#state.filename =/= undefined ->
@@ -73,10 +85,11 @@ callback(Next, State, Acc, Debug) ->
 			            NewState = State,
 				    NewAcc   = Acc;
 				true ->
-			            case file:open(State#state.filename, [raw, write]) of
+                                    TmpState = UpdateStateCallback(State),
+			            case file:open(TmpState#state.filename, [raw, write]) of
 				        {ok, File} ->
 			                    file:write(File, Body),
-				            NewState = State#state{file = File},
+				            NewState = TmpState#state{file = File},
 				            NewAcc   = Acc;
 				        {error, Error} ->
 				            %% TODO: 合理的记录错误日志
@@ -103,12 +116,12 @@ callback(Next, State, Acc, Debug) ->
 		    io:format("NewState: ~p~n", [NewState]),
 		    io:format("NewAcc: ~p~n", [NewAcc])
             end,
-            fun(N) -> callback(N, NewState, NewAcc, Debug) end;
+            fun(N) -> callback(N, NewState, NewAcc, Debug, UpdateStateCallback) end;
 	body_end ->
 	    if
 	        State#state.file =/= undefined ->
                     file:close(State#state.file),
-		    NewAcc = [{State#state.type, State#state.filename} | Acc];
+		    NewAcc = [{State#state.path, State#state.guid, State#state.type} | Acc];
 		true ->
 		    NewAcc = Acc
             end,
@@ -118,27 +131,31 @@ callback(Next, State, Acc, Debug) ->
 		    io:format("NewState: ~p~n", [#state{}]),
 		    io:format("NewAcc: ~p~n", [NewAcc])
 	    end,
-	    fun(N) -> callback(N, #state{}, NewAcc, Debug) end;
+	    fun(N) -> callback(N, #state{}, NewAcc, Debug, UpdateStateCallback) end;
 	eof ->
 	    lists:reverse(Acc);
 	_ ->
-	    fun(N) -> callback(N, State, Acc, Debug) end
+	    fun(N) -> callback(N, State, Acc, Debug, UpdateStateCallback) end
     end.
 
 %% Internal APIs:
 
 %% 绝对路径: ?NFS_PREFIX ++ State#path ++ "/ori/" State#guid ++ State#type
 update_state_photo(State) ->
-    NewState = State#state{path = woomsg_nfs_cache:get_photo_path(),
-                           guid = woomsg_guid:get_image_guid(),
-		 	   filename = ?NFS_PREFIX ++ State#state.path ++ "/ori/" ++ State#state.guid ++ State#state.type},
+    Path = woomsg_nfs_cache:get_photo_path(),
+    Guid = woomsg_guid:get_image_guid(),
+    NewState = State#state{path = Path,
+                           guid = Guid,
+		 	   filename = ?NFS_PREFIX ++ Path ++ "/ori/" ++ Guid ++ State#state.type},
     NewState.
 
 %% 绝对路径: ?NFS_PREFIX ++ State#path ++ "/ori/" State#guid ++ State#type
 update_state_pic(State) ->
-    NewState = State#state{path = woomsg_nfs_cache:get_pic_path(),
-                           guid = woomsg_guid:get_image_guid(),
-		 	   filename = ?NFS_PREFIX ++ State#state.path ++ "/ori/" ++ State#state.guid ++ State#state.type},
+    Path = woomsg_nfs_cache:get_pic_path(),
+    Guid = woomsg_guid:get_image_guid(),
+    NewState = State#state{path = Path,
+                           guid = Guid,
+		 	   filename = ?NFS_PREFIX ++ Path ++ "/ori/" ++ Guid ++ State#state.type},
     NewState.
 
 %%
@@ -157,7 +174,7 @@ verify_type_in_header(Headers) ->
 	{"image/gif", _} ->
 	    ".gif";
 	{"image/bmp", _} ->
-	    ".gif";
+	    ".bmp";
 	_ ->
 	    unknown
     end.
