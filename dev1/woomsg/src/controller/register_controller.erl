995f04e479e4184ba9c9f1d2f92adc8b9c5b6103
woomsgdev <woomsgdev@1c5293ca-d401-11de-9aee-2b7272db14c4>
2010-01-06 14:49:30 +0000

    test

--- dev1/woomsg/src/controller/register_controller.erl
+++ dev1/woomsg/src/controller/register_controller.erl
@@ -1,9 +1,23 @@
 -module(register_controller).
 -export([handle_get/1, handle_post/1]).
 
+-define(DEF_USERNAME, <<"请输入用户名">>).
+
 handle_get(Req) ->
-    Data = register_view:index("test data"),
-    Req:respond({200, [{"Content-Type", "text/html"}], Data}).
+    case woomsg_common:user_state(Req) of
+	{login, Username} ->
+	    Data = register_view:index(login, Username),
+	    Req:respond({200, [{"Content-Type", "text/html"}], Data});
+	{logout_remember, undefined} ->
+	    Data = register_view:index(login_remember, ?DEF_USERNAME),
+	    Req:respond({200, [{"Content-Type", "text/html"}], Data});
+	{logout_remember, Username} ->
+	    Data = register_view:index(logout_remember, Username),
+	    Req:respond({200, [{"Content-Type", "text/html"}], Data});
+	{logout_no_remember, undefined} ->
+	    Data = register_view:index(logout_no_remember, ?DEF_USERNAME),
+	    Req:respond({200, [{"Content-Type", "text/html"}], Data})
+    end.
 
 handle_post(_Req) ->
     ok.
