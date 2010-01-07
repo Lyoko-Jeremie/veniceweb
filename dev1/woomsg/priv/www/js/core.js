f0a1f95dcedf7018643dfce1d5d85ce1278d28cb
woomsgdev <woomsgdev@1c5293ca-d401-11de-9aee-2b7272db14c4>
2010-01-07 13:18:25 +0000

    test

--- dev1/woomsg/priv/www/js/core.js
+++ dev1/woomsg/priv/www/js/core.js
@@ -20,3 +20,60 @@ $(document).ready(function() {
 
 })
 
+/*
+ * 用户登录
+ */
+function header_login() {
+  $('#nav-login-button').hide();
+  $('#nav-login-ajax').show();
+  $.post("login/ajax", $('#header_login_form').serialize(), validate_header_login, "json");
+}
+
+function validate_header_login(data) {
+  $('#nav-login-button').show();
+  $('#nav-login-ajax').hide();
+  if(data.result == "error") {
+    $.achtung({message:data.content, 
+               timeout:3,
+	       className:'achtungFail'});
+  } 
+ 
+  else if (data.result == "ok") {
+    location.href = "user/" + data.content;
+  }
+}
+
+/*
+ * Cookie Helpers
+ */
+function createCookie(name,value,days)
+{
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime()+(days*24*60*60*1000));
+    var expires = "; expires="+date.toGMTString();
+  }
+  else var expires = "";
+  document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+function eraseCookie(name) {
+  createCookie(name,"",-1);
+} 
+
+
+
+
+
+
